f8f8eb48127f0f6ee671764685be67d5c42e6bc6
Dima <soloveid87@mail.ru>
2022-09-15 17:49:18 +1000

    apiService

---
+++ app/src/main/java/com/dima/cryptoapp/api/ApiService.kt
@@ -0,0 +1,33 @@
+package com.dima.cryptoapp.api
+
+import com.dima.cryptoapp.pojo.CoinInfoListOfData
+import com.dima.cryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "bf00560df591b890648b5c3143a8009f2cd3c7dfe6f635b9d256188b3dbdaae7",
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "bf00560df591b890648b5c3143a8009f2cd3c7dfe6f635b9d256188b3dbdaae7",
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
+    ): Single<CoinPriceInfoRawData>
+
+    companion object {
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+        private const val CURRENCY = "USD"
+    }
+}
