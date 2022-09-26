0518f9360470bd38de17fdc76278385225a6c82a
Dima <soloveid87@mail.ru>
2022-09-27 09:48:54 +1000

    les46 ready, maket item_coin_info

--- app/src/main/java/com/dima/cryptoapp/pojo/CoinPriceInfo.kt
+++ app/src/main/java/com/dima/cryptoapp/pojo/CoinPriceInfo.kt
@@ -2,6 +2,8 @@ package com.dima.cryptoapp.pojo
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.dima.cryptoapp.api.ApiFactory.BASE_IMAGE_URL
+import com.dima.cryptoapp.utils.convertTimestampToTime
 import com.google.gson.annotations.Expose
 
 import com.google.gson.annotations.SerializedName
@@ -36,7 +38,7 @@ data class CoinPriceInfo(
 
     @SerializedName("LASTUPDATE")
     @Expose
-    val lastupdate: Int?,
+    val lastupdate: Long?,
 
     @SerializedName("LASTVOLUME")
     @Expose
@@ -165,4 +167,12 @@ data class CoinPriceInfo(
     @SerializedName("IMAGEURL")
     @Expose
     val imageurl: String?,
-)
+) {
+    fun getFormattedTime(): String {
+        return convertTimestampToTime(lastupdate)
+    }
+
+    fun getFullImageUrl() : String{
+        return BASE_IMAGE_URL + imageurl
+    }
+}
