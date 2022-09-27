ac030831d9d4a01188051ef6b92b21e7c9f85ce0
Dima <soloveid87@mail.ru>
2022-09-27 13:00:38 +1000

    coin detail activity work

--- app/src/main/java/com/dima/cryptoapp/CoinDetailActivity.kt
+++ app/src/main/java/com/dima/cryptoapp/CoinDetailActivity.kt
@@ -1,10 +1,14 @@
 package com.dima.cryptoapp
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_coin_detail.*
 
 class CoinDetailActivity : AppCompatActivity() {
 
@@ -20,11 +24,23 @@ class CoinDetailActivity : AppCompatActivity() {
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL)
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.getDetailInfo(fromSymbol!!).observe(this, Observer {
-            Log.d("DETAIL_INFO", it.toString())
+            tvPrice.text = it.price.toString()
+            tvMinPrice.text = it.lowday.toString()
+            tvMaxPrice.text = it.highday.toString()
+            tvLastMarket.text = it.lastmarket
+            tvLastUpdate.text = it.getFormattedTime()
+            tvFromSymbol.text = it.fromsymbol
+            tvToSymbol.text = it.tosymbol
+            Picasso.get().load(it.getFullImageUrl()).into(ivLogoCoin)
         })
     }
 
     companion object{
-        const val EXTRA_FROM_SYMBOL = "fSym"
+        private const val EXTRA_FROM_SYMBOL = "fSym"
+        fun newIntent(context: Context, fromSymbol: String) : Intent {
+            val intent = Intent(context,CoinDetailActivity::class.java)
+            intent.putExtra(EXTRA_FROM_SYMBOL, fromSymbol)
+            return intent
+        }
     }
 }
--- app/src/main/java/com/dima/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/dima/cryptoapp/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.dima.cryptoapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -19,16 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val adapter = CoinInfoAdapter(this)
-        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener{
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
-               val intent = Intent(this@MainActivity,CoinDetailActivity::class.java)
-                intent.putExtra(CoinDetailActivity.EXTRA_FROM_SYMBOL,coinPriceInfo.fromsymbol)
+                val intent =
+                    CoinDetailActivity.newIntent(this@MainActivity, coinPriceInfo.fromsymbol)
                 startActivity(intent)
             }
         }
         rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        viewModel.priceList.observe(this,Observer{
+        viewModel.priceList.observe(this, Observer {
             adapter.coinInfoList = it
         })
     }
